f222e86a94a3fb598b0075af57b9e47943bd3a71
LiuYHCode <lyh997835@163.com>
2024-07-02 23:22:35 +0800

    【修改内容】:hello world案例搭建 【提交类型】:阶段提交 【项管系统】:无 【详细描述】:hello world案例搭建 【需要测试】:是

--- rabbitmq_basics/src/main/java/com/at2024/rabbitmq_basics/RabbitmqBasicsApplication.java
+++ rabbitmq_basics/src/main/java/com/at2024/rabbitmq_basics/RabbitmqBasicsApplication.java
@@ -1,13 +1,13 @@
-package com.at2024.rabbitmq_study;
+package com.at2024.rabbitmq_basics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RabbitmqStudyApplication {
+public class RabbitmqBasicsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RabbitmqStudyApplication.class, args);
+        SpringApplication.run(RabbitmqBasicsApplication.class, args);
     }
 
 }
